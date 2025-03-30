f1f76072ce2a3274c597a8a5b7e4b22f465279d5
Giuseppe Di Bona <giusedb@gmail.com>
2025-03-30 19:14:20 +0200

    complete basig API

--- quasar_api/resources/db.py
+++ quasar_api/resources/db.py
@@ -1,13 +1,21 @@
 import asyncio
-from operator import attrgetter, itemgetter
+from functools import reduce
 from typing import List
 
 from sqlalchemy import select
-from sqlalchemy.orm import DeclarativeBase, InstrumentedAttribute
-from sqlalchemy.sql.operators import eq
+from sqlalchemy.orm import DeclarativeBase, InstrumentedAttribute, Relationship, ColumnProperty, RelationshipDirection
+from sqlalchemy.sql.operators import eq, contains, and_
 
 from .base import WebResource, verb
 from ..context import db
+
+from pluralizer import Pluralizer
+
+from ..exceptions import RecordNotFound
+from ..utils import col2attr
+
+pluralizer = Pluralizer()
+pluralize = pluralizer.plural
 
 def to_js_type(field_type) -> str:
     """Transform a SQLAlchemy type into a JS type adapter."""
@@ -15,98 +23,172 @@ def to_js_type(field_type) -> str:
         return field_type.__name__.lower()
     return type(field_type).__name__.lower()
 
-def attributes(model) -> dict[str, InstrumentedAttribute]:
-    """Introspect a model and return a dictionary of DB attributes."""
-    attrs = ((name, getattr(model, name)) for name in dir(model))
-    return {name: attr for name, attr in attrs
-            if isinstance(attr, InstrumentedAttribute)}
 
 class DBResource(WebResource):
     """Web Resource based on sqlalchemy model."""
 
     def __init__(self, resource_manager: 'ResourceManager', name: str,
-                 model: DeclarativeBase, permissions: dict = None):
+                 model: DeclarativeBase, permissions: dict = None, columns: list = None):
         super(DBResource, self).__init__()
         self.name = name
         self.model = model
         self._permissions = permissions or {}
         self.resource_manager = resource_manager
+        self.columns = columns or [col.name for col in self.model.__table__.columns]
+        def serialize(obj):
+            return {col: getattr(obj, col) for col in self.columns}
+        model.__serialize__ = serialize
+        self.pk = model.__mapper__.primary_key[0]
 
     @property
     def one_to_many(self) -> List[dict]:
         """Checks all the "One-to-many" relations."""
+
+        def get_attribute_name(self, fk):
+            return pluralize(self.resource_manager.tables[fk.column.table.name].name)
+
+        def serialize(name, field):
+            return {'resource': self.resource_manager[field.prop.argument],
+                     'type': 'in',
+                     'foreign_field': next(iter(field.prop.synchronize_pairs))[0].name,
+                     'attribute': name,
+                     'description': 'TODO'}
+
         return [
-            {'resource': self.resource_manager.tables[fk.column.table.name].name,
-             'description': 'TODO',
-             'type': 'in',
-             'attribute': fk.constraint.column_keys}
-            for fk in self.resource_manager.foreign_keys
-            if fk.constraint.table == self.model.__table__
+            serialize(name, field)
+            for name, field in self._relationships()
+            if field.prop.argument in self.resource_manager
         ]
 
     @property
     def many_to_one(self) -> List[dict]:
+
+        def serialize(fk):
+            return {'resource': self.resource_manager.tables[fk.constraint.table.name].name,
+                     'type': 'out',
+                     'foreign_field': fk.constraint.name,
+                     'attribute': fk.column.table.name,
+                     'description': 'TODO'}
+
         return [
-            {'resource': self.resource_manager.tables[fk.constraint.table.name].name,
-             'description': 'TODO',
-             'type': 'out',
-             'attribute': fk.column.table.name}
-            for fk in self.resource_manager.foreign_keys
+            serialize(fk)
+        for fk in self.resource_manager.foreign_keys
             if fk.column.table == self.model.__table__
         ]
-
 
     @property
     def references(self) -> List[dict]:
         """List all the relations for this Model."""
-        return list(sorted(self.one_to_many + self.many_to_one, key=itemgetter('resource')))
 
+        # return list(sorted(self.one_to_many + self.many_to_one, key=itemgetter('resource')))
+        def get_remote(prop):
+            pair = tuple(prop.remote_side)
+            if len(pair) == 1:
+                return pair[0]
+            if next(iter(pair[0].foreign_keys)).constraint.referred_table == self.model.__table__:
+                local, remote = pair
+            else:
+                local, remote = reversed(pair)
+            return remote
+
+        def resolve(prop) -> str:
+            if prop.direction == RelationshipDirection.MANYTOMANY:
+                table_name = next(iter(get_remote(prop).foreign_keys)).constraint.referred_table.name
+            else:
+                table_name = next(iter(prop.remote_side)).table.name
+            return self.resource_manager.tables[table_name]
+
+        def serialize(name, prop):
+            directions = {
+                RelationshipDirection.MANYTOONE: 'in',
+                RelationshipDirection.ONETOMANY: 'out',
+                RelationshipDirection.MANYTOMANY: 'bi',
+            }
+
+            return dict(
+                resource=resolve(prop).name,
+                type=directions[prop.direction],
+                attribute=name,
+                foreign_attribute=col2attr(resolve(prop).model)[get_remote(prop).name],  # TODO multifields
+                description=prop.doc,
+                local_attribute=col2attr(self.model)[next(iter(prop.local_columns)).name],
+            )
+
+        for prop in self.model.__mapper__.relationships:
+            if prop.direction == RelationshipDirection.MANYTOMANY:
+                continue
+            tab_name = get_remote(prop).table.name
+            if tab_name not in self.resource_manager.tables:
+                continue
+            yield serialize(prop.key, prop)
 
     @property
     def description(self):
+
+        def serialize(name, field):
+            return {
+                'name': name,
+                'description': field.doc,
+                'type': to_js_type(field.type),
+                'extra': {},
+                'validators': [],  # TODO add validators
+            }
+
         ret = {}
         ret['name'] = self.name
         ret['description'] = self.model.__doc__
-        ret['permissions'] = self._permissions or []
-        ret['fields'] = [{
-            'name': field.name,
-            'description': field.comment,
-            'type': to_js_type(field.type),
-            'widget': None,
-            'constraints': ['TODO'],
-            'validators': [],
-            } for field in self.model.__table__.columns]
+        # ret['permissions'] = self._permissions or []
+        ret['fields'] = [serialize(col.key, col) for col in self.model.__mapper__.columns]
         ret['UID'] = [f.name for f in self.model.__table__.primary_key]
-        ret['references'] = self.references
+        ret['references'] = tuple(self.references)
         return ret
 
     @verb
     async def describe(self):
         await asyncio.sleep(0)
-        return self.description
+        return {"DESCRIPTION": self.description}
+
+    async def by_pk(self, pk):
+        """Get the record object by its primary key."""
+        return (await db.execute(select(self.model).where(self.pk == pk))).scalar()
 
     @verb
     async def get(self, filter: dict | None = None) -> list[DeclarativeBase]:
         """Returns the list of `model`."""
         query = select(self.model)
         if filter:
-            query.where(*(eq(getattr(self.model, name), value) for name, value in filter.items()))
+            query = query.where(reduce(and_, (
+                getattr(self.model, name).in_(val) for name, val in filter.items())))
         data = (await db.execute(query))
         return data.scalars().all()
 
     @verb
     async def post(self, record: dict) -> None:
         """Create a new `model` instance on on the database."""
-        instance = self.model(**record)
-        context.context.db.add(instance)
-        await context.context.db.commit()
-
+        item = self.model(**record)
+        db.add(item)
+        await db.flush()
+        return item
 
     @verb
-    def put(self, pk: str, record: dict) -> None:
+    async def put(self, pk: str, record: dict) -> None:
         """Update the record on the DB."""
+        rec = await self.by_pk(pk)
+        if not rec:
+            raise RecordNotFound(f'Record {pk} not found')
+        for attr, value in record.items():
+            # TODO limit the updates to the writable fields
+            setattr(rec, attr, value)
+        db.flush()
+        return rec
 
 
     @verb
-    def delete(self, pk: str) -> None:
+    async def delete(self, pk: str) -> None:
         """Delete the record on the DB."""
+        rec = await self.by_pk(pk)
+        if not rec:
+            raise RecordNotFound(f'Record {pk} not found')
+        db.delete(rec)
+        await db.flush()
+        return rec
--- quasar_api/resources/manager.py
+++ quasar_api/resources/manager.py
@@ -1,12 +1,21 @@
+import time
+from collections import defaultdict
+from typing import Iterable
+
 from click import style
 from redis import Redis
+from sqlalchemy.ext.asyncio import AsyncSession
 from typing_extensions import Callable
 
 from quasar_api.context.manager import ContextManager
+from quasar_authentication.manager import AuthenticationManager
+from quasar_authorization.models import UserMixin
 from .base import WebResource  # pylint disable=relative-beyond-top-level
 from .db import DBResource
 from ..exceptions import ResourceNotFoundException
 import logging
+
+from ..utils import JSONMixin
 
 logger = logging.getLogger(__package__)
 
@@ -17,9 +26,15 @@ class ResourceManager:
     resources: dict = {}
     tables: dict = {}
 
-    def __init__(self, session_maker: Callable | None = None,
-                 redis_connection: Redis | str | None = None):
+    def __init__(self,
+                 auth_man: AuthenticationManager,
+                 session_maker: Callable | None = None,
+                 redis_connection: Redis | str | None = None,
+                 name: str | None = None):
         self.context = ContextManager(self, session_maker, redis_connection)
+        self.auth_man = auth_man
+        self.last_run = time.time()
+        self.app_name = name or 'no-name'
 
     def __call__(self, token=None):
         return self.context(token)
@@ -28,6 +43,8 @@ class ResourceManager:
         """Register a web resource for getting exposed to the web endpoints."""
         self.resources[resource.name] = resource
         if isinstance(resource, DBResource):
+            self.resources[resource.model] = resource
+            self.resources[resource.model.__table__] = resource
             self.tables[resource.model.__table__.name] = resource
 
     @property
@@ -37,7 +54,20 @@ class ResourceManager:
                 if fk.constraint.table.name in self.tables
                 and fk.column.table.name in self.tables)
 
-    async def action(self, token: str, resource: str, verb: str, *args, **kwargs):
+    def serialize_results(self, result: Iterable) -> dict:
+        if not result:
+            return {}
+        if isinstance(result, dict):
+            return result
+
+        ret = defaultdict(list)
+        if not isinstance(result, Iterable):
+            result = [result]
+        for item in result:
+            ret[self[item.__table__].name].append(item.to_dict())
+        return dict(ret)
+
+    async def action(self, token: str, resource: str, verb: str, *args, **kwargs) -> dict:
         """Finds the correct `resource` and call the right `verb` with `args`."""
         logger.info(f'received request to {style(verb, 'red')} on {style(resource, 'blue')} from {style(token, 'yellow')}.')
         res = self.resources.get(resource)
@@ -47,9 +77,21 @@ class ResourceManager:
         if not action:
             raise ResourceNotFoundException(f'Verb {resource}.{verb} not found')
 
-        async with self.context(token):
+        async with self.context(token) as ctx:
             result = await action(*args, **kwargs)
-            return result
+            return self.serialize_results(result)
+
+    async def login(self, username: str, password: str) -> dict | None:
+        """Log in the user and return the status object."""
+        user = await self.auth_man.login(username, password)
+        token, _ = await self.context.web_session_man.new()
+        if user:
+            return {
+                'user': user.to_dict(),
+                'last_build': self.last_run,
+                'token': token,
+                'application': self.app_name
+            }
 
     def expose(self, model):
         """Model decorator to register the model"""
@@ -60,3 +102,7 @@ class ResourceManager:
     def __getitem__(self, item: str) -> WebResource:
         """Checks if there is a `Resource` or a `table` with that nema and return the `Resouce"""
         return self.resources.get(item) or self.tables.get(item)
+
+    def __contains__(self, item):
+        """Check if the resource is in the resource list"""
+        return item in self.resources
